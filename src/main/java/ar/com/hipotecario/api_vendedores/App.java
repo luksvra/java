f8f4a64cd8b356caf74dc0372beb64cdd472cf7b
luksvra <lucasvera@outlook.com>
2019-10-09 17:08:56 -0300

    version inicial

---
+++ src/main/java/ar/com/hipotecario/api_vendedores/App.java
@@ -0,0 +1,13 @@
+package ar.com.hipotecario.api_vendedores;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
